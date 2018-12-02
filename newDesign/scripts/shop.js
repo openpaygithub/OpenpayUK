10dbe1f539ef1fea37b27a81e596fe88bd8f09c9
Kfir Miller <kfir_m@optimove.com>
2018-12-03 00:05:39 +0200

    index.html page - change

--- newDesign/scripts/shop.js
+++ newDesign/scripts/shop.js
@@ -23,6 +23,7 @@ $('#categories-grid').on('click', '.marketToProvide', function (e) {
     if (CategoryID) {
         openpayUtils.modifySearchParams({CategoryID: CategoryID});
         openpayUtils.searchBrands();
+        $("#category").val(CategoryID);
     }
 });
 
