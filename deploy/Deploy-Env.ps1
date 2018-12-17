0420dbd31c32bb80b205369475f7e78abbb5bb13
Indika Udagedara <indika.udagedara@gmail.com>
2018-12-17 13:19:59 +1100

    Deploy script

---
+++ deploy/Deploy-Env.ps1
@@ -0,0 +1,37 @@
+param([string] $env, [string] $dir)
+
+if ($env -eq "Dev")
+{
+    $hostingBucket = "s3://www.dev.openpay.com.au"
+}
+elseif ($env -eq "ProdAU")
+{
+    $hostingBucket = "s3://www.openpay.com.au"
+}
+else
+{
+    Write-Host "Invalid account"
+    Exit 1
+}
+
+$cwd = Get-Location
+Set-Location $dir
+$files = Get-ChildItem -File -Filter "*.html" -Recurse
+
+aws s3 sync --acl public-read $dir $hostingBucket
+
+ForEach ($f in $files)
+{
+    $newfile = $f.FullName -replace ".html", ""
+    cp $f.FullName $newfile
+
+
+    $relativePath = Get-Item $newfile | Resolve-Path -Relative 
+    $s3Key = $relativePath -replace "\.\\", "" -replace "\\", "/"
+    
+    aws s3 cp --content-type "text/html" --acl public-read $newfile $hostingBucket/$s3Key
+}
+
+
+cd $cwd
+
