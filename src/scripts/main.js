cb855e46d9abccd04bd34faf1fff1fded6c10ed9
Vladyslav Soloviov <vladyslavs@openpay.com.au>
2019-05-27 17:01:14 +0300

    WIP add How it works page and related links in header and footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ function triggerMenu() {
   btnTrigger.addEventListener('click', () => {
     if (menuBlock.classList[1] === 'header-nav_disable') {
       menuBlock.classList.remove('header-nav_disable');
-      header.style.cssText = 'height: 200px';
+      header.style.cssText = 'height: 220px';
     } else {
       menuBlock.classList.add('header-nav_disable');
       header.style.cssText = 'height: 115px';
