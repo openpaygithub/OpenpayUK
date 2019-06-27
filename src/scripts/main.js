1ec1547ded4af7662c7ffea8cd02fc7261999327
Vladyslav Soloviov <vladyslavs@openpay.com.au>
2019-06-27 07:42:52 +0300

    update nav menu styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,23 @@
 function triggerMenu() {
   const btnTrigger = document.querySelector('.trigger-menu');
   const menuBlock = document.querySelector('.header-nav');
-  const header = document.querySelector('.header-wrap');
+  const headerWrap = document.querySelector('.header-wrap');
+  const header = document.querySelector('.header');
+  const page = document.querySelector('body');
 
   btnTrigger.addEventListener('click', () => {
     if (menuBlock.classList[1] === 'header-nav_disable') {
       menuBlock.classList.remove('header-nav_disable');
       btnTrigger.classList.add('trigger-menu--active');
-      header.style.cssText = 'height: 260px';
+      header.classList.add('header--border');
+      page.classList.add('page--mobile');
+      headerWrap.style.cssText = 'height: 100%';
     } else {
       menuBlock.classList.add('header-nav_disable');
       btnTrigger.classList.remove('trigger-menu--active');
-      header.style.cssText = 'height: 90px';
+      header.classList.remove('header--border');
+      page.classList.remove('page--mobile');
+      headerWrap.style.cssText = 'height: 60px';
     }
   });
 }
